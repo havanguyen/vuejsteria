9538eaa79e9391a5dd7216531b77819b3350ea34
havanguyen <hvnguyen902993@gmail.com>
2025-11-11 20:34:04 +0700

    ok

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = 'http://localhost:9000/api/v1';
+//export const API_BASE_URL = 'http://localhost:9000/api/v1';
+export const API_BASE_URL = 'http://api.bookteria.xyz/api/v1';
